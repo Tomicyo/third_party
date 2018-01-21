3e795c3a36ad5741cc2dd39a8aa30103af25f195
Tomicyo <dsotsen@gmail.com>
2018-01-21 21:30:45 +0800

    update ndk to r16b

--- tools/appveyor/install_android.bat
+++ tools/appveyor/install_android.bat
@@ -2,8 +2,8 @@ pushd %~dp0
 cd /
 appveyor DownloadFile http://dl.google.com/android/android-sdk_r24.3.4-windows.zip
 7z x android-sdk_r24.3.4-windows.zip > nul
-appveyor DownloadFile https://dl.google.com/android/repository/android-ndk-r15c-windows-x86_64.zip
-7z x android-ndk-r15c-windows-x86_64.zip
+appveyor DownloadFile https://dl.google.com/android/repository/android-ndk-r16b-windows-x86_64.zip
+7z x android-ndk-r16b-windows-x86_64.zip
 appveyor DownloadFile https://dl.google.com/android/repository/cmake-3.6.4111459-windows-x86_64.zip
 7z x cmake-3.6.4111459-windows-x86_64.zip -oC:\android-sdk-windows\cmake\3.6.4111459
 @rem echo y | C:\android-sdk-windows\tools\android.bat update sdk --no-ui --all --filter android-23
